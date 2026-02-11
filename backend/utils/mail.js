41b44d8f03cc7f79cabaf536e642557fd6762c6e
Devansh Kumar <devanshkumar9931@gmail.com>
2026-02-11 20:02:18 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,27 +1,13 @@
-import nodemailer from "nodemailer"
-import dotenv from "dotenv"
-dotenv.config()
-console.log("MAIL CONFIG CHECK:", {
-  EMAIL: process.env.EMAIL,
-  HAS_APP_PASSWORD: !!process.env.EMAIL_APP_PASSWORD,
-});
+import { Resend } from "resend";
+import dotenv from "dotenv";
+dotenv.config();
 
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_APP_PASSWORD,
-  },
-});
-
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOtpMail = async (to, otp) => {
   try {
-    await transporter.sendMail({
-      from: `"MealMate" <${process.env.EMAIL}>`,
+    await resend.emails.send({
+      from: "MealMate <onboarding@resend.dev>",
       to,
       subject: "MealMate : Reset Your Password",
       html: `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`,
@@ -34,8 +20,8 @@ export const sendOtpMail = async (to, otp) => {
 
 export const sendDeliveryOtpMail = async (user, otp) => {
   try {
-    await transporter.sendMail({
-      from: `"MealMate" <${process.env.EMAIL}>`,
+    await resend.emails.send({
+      from: "MealMate <onboarding@resend.dev>",
       to: user.email,
       subject: "MealMate : Delivery OTP",
       html: `<p>Your OTP for Delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`,
